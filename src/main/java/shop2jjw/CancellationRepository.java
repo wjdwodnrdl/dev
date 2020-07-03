a1ed5823eccd424bd3b2d99aeca6a62dee80be54
SKCC <SKCC@SKCC16D00078.SKCC.NET>
2020-07-03 14:06:25 +0900

    first commit

---
+++ src/main/java/shop2jjw/CancellationRepository.java
@@ -0,0 +1,8 @@
+package shop2jjw;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
+
+
+}
---
+++ src/main/java/shop2jjw/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package shop2jjw;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
